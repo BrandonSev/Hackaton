7e3eb7ad7adb389541b238159b20f49e88893e31
SaradinNicolas <saradinnicolas9@gmail.com>
2021-11-26 11:42:04 +0100

    Logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './Header.css';
 
 function Header() {
   return (
     <div className="d-flex justify-content-between align-items-center mt-2">
-      <div className="font-weight-bold">Let's the Music Play</div>
+      <div ><img className="logo" src="/image/Logo.png" alt="" /></div>
       <div className="d-flex align-items-center">
         <button className="btn btn-primary">Login</button>
         <button className="btn btn-primary ml-2">Signup</button>
